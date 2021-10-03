a51458df7e3784a5c39ca31b4a6519816eb8b911
turn <kksy2423@naver.com>
2021-10-04 03:17:24 +0900

    clone(background)

---
+++ js/background.js
@@ -0,0 +1,18 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpeg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpeg",
+    "6.jpeg",
+]
+
+
+const chosenImage= images[Math.floor(Math.random()*images.length)];
+
+const bgImage=document.createElement("img");
+
+bgImage.src=`img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
