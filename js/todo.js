58aa106ad7550dc907b9253e8200cc50042c1c49
turn <kksy2423@naver.com>
2021-10-06 02:45:26 +0900

    weather (css로 마무리 하기)

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList= document.getElementById("todo-list");
 
 const TODOS_KEY="todos";
 
-const toDos=[];
+let toDos=[];
 
 function savedTodos(){
     localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
@@ -14,26 +14,35 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj= {
+        text: newTodo,
+        id:Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    savedTodos();
 }
 
 function paintToDo(newTodo){
     const li=document.createElement("li");
+    li.id=newTodo.id;
     const span=document.createElement("span");
-    span.innerText=newTodo;
+    span.innerText=newTodo.text;
     const button=document.createElement("button");
     button.innerText="❌"
     button.addEventListener("click",deleteTodo)
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
-    savedTodos();
+    
+   
 }
 
 function deleteTodo(event){
     const li =event.target.parentElement;
+    toDos=toDos.filter(toDo => toDo.id !== parseInt(li.id) )
     li.remove();
+    savedTodos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit)
@@ -43,7 +52,8 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos!=null){
     const parsedToDOs=JSON.parse(savedToDos);
-    parsedToDOs.array.forEach(item => {
-        
+    toDos=parsedToDOs;
+    parsedToDOs.forEach(element => {
+        paintToDo(element);
     });
 }
