1b4288e87373a3f1dd9d2a524cd69ebe295ac384
turn <kksy2423@naver.com>
2021-10-08 02:41:04 +0900

    css 거의 완성(날씨만 css 만들기)

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,23 @@
-const clock=document.querySelector("h2#clock");
+const week=["SUN","MON","TUE","WED","THU","FRI","SAT"];
 
+const clock=document.querySelector("h2#clock");
+const Fulldate=document.createElement("span");
+const time=document.createElement("span");
+Fulldate.id="date";
+time.id="time";
+clock.appendChild(Fulldate);
+clock.appendChild(time);
 function getClock(){
     const date=new Date();
+    const year=String(date.getFullYear()).padStart(2,"0");
+    const month=String(date.getMonth()+1).padStart(2,"0");
+    const day=String(date.getDate()).padStart(2,"0");
+    const mon=String(week[date.getDay()]);
     const hours=String(date.getHours()).padStart(2,"0");
     const minutes=String(date.getMinutes()).padStart(2,"0");
     const seconds=String(date.getSeconds()).padStart(2,"0");
-    clock.innerText=`${hours}:${minutes}:${seconds}`;
+    Fulldate.innerText=`${year}-${month}-${day}-${mon}`;
+    time.innerText=`${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
