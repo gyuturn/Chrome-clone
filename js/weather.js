ad03c8db11b42a60f265cf9c327c3d727df994b4
turn <kksy2423@naver.com>
2021-10-09 03:10:27 +0900

    날씨 css 구현완료(arrow 기능 추가예정)

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,28 @@
 const API_KEY="a5822fd600723ecbc217150c78c0f6a8";
+const icon=document.getElementById("icon");
+const img=document.createElement("img");
+const simpledescript=document.getElementById("simpledescript");
+const  city=document.getElementById("city");
 
 
 function onGeoOk(position){
     const lat= position.coords.latitude;
     const lng=position.coords.longitude;
-    console.log("You live in",lat ,lng);
     const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    
     fetch(url).then(response => response.json()).then(data => {
-        const weather= document.querySelector("#weather span:first-child");
-        const city= document.querySelector("#weather span:last-child");
-
-        city.innerText =data.name;
-        weather.innerText= `${data.weather[0].main} / ${data.main.temp}`;
+        const weatherimg = "http://openweathermap.org/img/wn/"+data.weather[0].icon+".png";
+        icon.appendChild(img);
+        img.src=weatherimg;
+        simpledescript.innerText =`${data.weather[0].main}`;
+        city.innerText=`${data.name}`;
+        
     })
 }
 function onGeoError(){
     alert("Can't find you. No weather for you");
 }
+
+
 
 navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
